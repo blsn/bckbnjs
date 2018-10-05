06737c6fc054076a4f86ce20080ea040a71d7356
mike <blsn2000@gmail.com>
2018-10-05 20:19:32 +0300

    backbone

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.3.3/underscore-min.js" type="text/javascript"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/backbone.js/0.9.2/backbone-min.js" type="text/javascript"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/backbone-localstorage.js/1.0/backbone.localStorage-min.js" type="text/javascript"></script> 
+    <script src="2_introduction_to_models.js"></script>
+</head>
+<body>
+    <h1>BackboneJS</h1>
+    <ul>
+        <li><a href="./">Installation</a></li>
+        <li><a href="2_introduction_to_models.php">2_introduction_to_models.php</a></li>
+    </ul>
+</body>
+</html>
