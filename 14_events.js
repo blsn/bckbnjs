627c1a7d883291da019be0b16a30856b7751080e
Moshe Balasiano <mbalasiano@leolane.com>
2018-10-07 10:05:11 +0300

    backbone

---
+++ 14_events.js
@@ -0,0 +1,33 @@
+function Function14(){
+/*
+    StudentModel = Backbone.Model.extend({
+        defaults: {
+            name: "unknown"
+        }
+    });
+
+    StudentCollection = Backbone.Collection.extend({
+        model: StudentModel,
+        initialize: function(){
+        }
+    });
+
+    $(document).ready(function(){
+        var jony = new StudentModel({name: "jony", id: 1});
+        var vicky = new StudentModel({name: "vicky", id: 2});
+        var student_group = new StudentCollection([jony, vicky]);        
+        displayCollectionContents("Before:", student_group); 
+        
+        var student = student_group.get(1);
+        student.set({name: "lopez"});
+        displayCollectionContents("After #1:", student_group); 
+
+        jony.set({name: "jony"});
+        displayCollectionContents("After #2:", student_group); 
+    });
+
+    function displayCollectionContents(string, collection){
+        console.log(string + " " + JSON.stringify(collection.toJSON()));
+    };
+*/
+}
--- index.php
+++ index.php
@@ -31,6 +31,7 @@
     <script src="11_introduction_to_collections.js"></script>
     <script src="12_adding_and_removing_elements.js"></script>
     <script src="13_modifying_elements.js"></script>
+    <script src="14_events.js"></script>
 </head>
 <body>
     <h1>BackboneJS</h1>
@@ -48,6 +49,7 @@
         <li><a href="#" onclick="Function11(); return false;">11 introduction to collections</a></li>
         <li><a href="#" onclick="Function12(); return false;">12 (Collections) adding and removing elements</a></li>
         <li><a href="#" onclick="Function13(); return false;">13 (Collections) modifying elements</a></li>
+        <li><a href="#" onclick="Function14(); return false;">14 (Collections) events</a></li>
     </ul>
     <div id="unique7"></div>
     <div id="unique8"></div>
